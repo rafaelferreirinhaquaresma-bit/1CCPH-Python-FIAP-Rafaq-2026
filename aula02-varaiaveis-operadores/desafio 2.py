786435c2572f1a259b06332f8e5cf832a8752f98
rafaelquaresma <rafaelferreirinhaquaresma@gmail.com>
2026-03-31 10:27:13 -0300

    Aula 3 - desafio

--- aula02-varaiaveis-operadores/desafio 2.py
+++ aula02-varaiaveis-operadores/desafio 2.py
@@ -1,5 +1,5 @@
 #desafio 2
 dia = int(input("digite o dia de nascimento: "))
-mes = int(input("digite o m~es de nascimento: "))
+mes = int(input("digite o mês de nascimento: "))
 ano = int(input("digite o ano de nascimento: "))
 print (f"a data de nascimento é: {dia}/{mes}/{ano}")
