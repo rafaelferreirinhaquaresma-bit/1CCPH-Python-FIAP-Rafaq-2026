2129fdf7c1f5128c57368707dcfd52f28b08789e
rafaelferreirinhaquaresma-bit <rafaelferreirinhaquaresma@gmail.com>
2026-09-15 09:47:35 -0300

    continuação da aula passada

--- Aula08-mini-crm/app.py
+++ Aula08-mini-crm/app.py
@@ -1,4 +1,7 @@
+from idlelib import query
 from os import name
+from re import search
+
 from model import model_lead
 import control
 def add_lead():
@@ -20,7 +23,6 @@ def add_lead():
 
     control.create_lead(model_lead(name, email, company, stage))
 
-
 def list_leads():
     leads = control.read_leads()
 
@@ -32,12 +34,34 @@ def list_leads():
     for i, lead in enumerate(leads):
         print(f"{i:02d} : {lead["nome"]:<20} {lead["email"]:<17}  {lead["company"]: <20}")
 
+def search_leads():
+    query = input("Buscar por: ").strip().lower()
+    if not query:
+        print("consulta vazia")
+        return
+# nesse mon=mento irei enviar minha usca para control
+# o control.read_lead_search
+leads_finded = control.search_leads(query)
+
+print(f"\n# |{"Nome":<20}| {Empresa:<}             | E-mail")
+for i, lead in enumerate(leads_finded):
+    print(f"{i:02d} : {lead["nome"]:<20} {lead["company"]:<17}  {lead["email"]: <20}")
+
+def export_leads():
+    path_csv = control.export_csv()
+
+    if path_csv is None:
+        print("nenhum lead nao encontrado")
+    else:
+        print(f"CSV exportado para {path_csv}")
 
 def main():
     while True:
         print("\nMini CRM - 1º Aula - (adicionar/listar")
         print("[1] Adicionar lead")
         print("[2] Listar lead")
+        print("[3] Buscar (nome, email, empresa)")
+        print("[4] Listar lead")
         print("[0] Sair")
 
         cpt = input("Escolha uma opcao: ")
@@ -45,6 +69,10 @@ def main():
             add_lead()
         elif cpt == "2":
             list_leads()
+        elif cpt == "3":
+            search.leads()
+        elif cpt == "4":
+            export.leads()
         elif cpt == "0":
             print("Sair")
             break
--- Aula08-mini-crm/control.py
+++ Aula08-mini-crm/control.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import json
+import json, csv
 
 DATA_DIR = Path(__file__).resolve().parent
 DATA_DIR.mkdir(exist_ok=True)
@@ -26,4 +26,34 @@ def create_lead(lead_dict):
 
     DB_PATH.write_text(json.dumps(leads, ensure_ascii=False, indent=2), encoding="utf-8")
 
-print(read_leads())
+def read_lead_search(query):
+    leads = read_leads() # retorna lista de leads
+    results = []
+
+    for i, lead in enumerate(leads):
+        txt_lead = f"{lead["name"]} {lead["company"]} {lead["email"]}".lower()
+
+        if query in txt_lead:
+            results.append(lead)
+    if not  results:
+        print("Nada encontrado")
+        return[]
+    else:
+        return results
+
+def export_csv():
+    #Exporta os leads para um CSV e retorna o path de onde o arquivo foi salvo
+    path_csv = DATA_DIR / "leads.csv"
+
+    leads = read_leads()
+
+    try:
+        with path_csv.open("w", newline="", encoding="utf-8" ) as file:
+             writer = csv.DictWriter(file, fieldnames= leads[0].keys())
+             writer.writeheader()
+             for row in leads:
+                writer.writerow(row)
+        return path_csv
+    except PermissionError:
+        return None
+print(read_lead_search("Nubank"))
